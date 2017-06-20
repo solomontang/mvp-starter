f0aec9b221ac3a9894944851e73ce3af85906abe
Solomon Tang <solomoncztang@gmail.com>
2017-06-20 09:15:41 -0700

    Render basic pentagon dynamically by stats

--- react-client/src/components/Pentagon.jsx
+++ react-client/src/components/Pentagon.jsx
@@ -10,27 +10,32 @@ class Pentagon extends React.Component {
   }
 
   drawPentagon() {
-    console.log('draw');
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     // console.log(Math.sin(72/180 * Math.PI) * 200 + 200));
     // console.log(200 - Math.cos(72/180 * Math.PI) * 200));
     ctx.beginPath();
-    ctx.moveTo(200, 200);
-    ctx.lineTo(200,0);
-    ctx.lineTo(Math.sin(18/180 * Math.PI) * 200 + 200, 200 + Math.cos(18/180 * Math.PI) * 200);
+    ctx.translate(200,20);
+    var stats = [1,1,1,1,1];
+    for (var i = 0; i < 5; i++) {
+      // console.log((18+i*72));
+      var x = Math.cos((18+i*72)/180 * Math.PI) * 200 * stats[i];
+      var y = 200 - Math.sin((18+i*72)/180*Math.PI) * 200 * stats[i];
+      console.log('x: ' + x, 'y: ' + y);
+      ctx.lineTo(x, y);
+    }
+    ctx.fillStyle = 'rgba(255, 165, 0, 0.5)';
     ctx.fill();
+
+
     // // ctx.lineTo(Math.cos(72/180 * Math.PI)*200, Math.sin(72/180 * Math.PI)*200);
     // ctx.lineTo(400,400);
     // ctx.fill();
-    // ctx.fillRect(25, 25, 100, 100);
-    // ctx.clearRect(45, 45, 60, 60);
-    // ctx.strokeRect(50, 50, 50, 50);
   }
 
   render() {
     return (<div>
-        <canvas id='canvas'></canvas>
+        <canvas id='canvas' width="400" height ="400"></canvas>
     </div>)
   }
 }
